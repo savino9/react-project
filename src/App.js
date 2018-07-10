46c2b6a089163cbdd38db776f818feb4a8f6cc08
Savino Rasola <savinorasola90@gmail.com>
2018-07-10 20:16:16 +0200

    scroll component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-// import {robots} from './robots';
+import Scroll from './Scroll';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -28,13 +28,19 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
 		})
-		return(
-			<div className='tc'>
-			<h1 className='f1'>Robot Friends</h1>
-			<SearchBox searchChange={this.onSearchChange} />
-			<CardList robots={filteredRobots} />
-			</div>
-		); 
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return(
+				<div className='tc'>
+				<h1 className='f1'>Robot Friends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
+					<CardList robots={filteredRobots} />
+				</Scroll>
+				</div>
+			);
+		}
 	}
 }
 
