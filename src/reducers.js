c7222d7c9850097eab114a5a0af45257c90dc715
Savino Rasola <savinorasola90@gmail.com>
2018-07-11 20:52:14 +0200

    cleaning up code

--- src/reducers.js
+++ src/reducers.js
@@ -5,10 +5,9 @@ const initialState = {
 }
 
 export const searchRobots = (state=initialState, action={}) => {
-	console.log(action.type);
 	switch(action.type){
 		case CHANGE_SEARCH_FIELD: 
-			return Object.assign({}, state, {searchField:action.payload});
+			return Object.assign({}, state, { searchField: action.payload });
 		default:
 			return state;
 	}
