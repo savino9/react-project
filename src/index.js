42600c3afa86d04e356e0afde8bcc957e14289ec
Savino Rasola <savinorasola90@gmail.com>
2018-07-11 20:02:17 +0200

    connecting Redux with React, wrapping the App component into Provider component wich is the responsable to give the store to all the children components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// connecting React->Redux
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+// 
+
 import './index.css';
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker';
+import {searchRobots} from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots)
 
-ReactDOM.render(<App /> ,document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App /> 
+	</Provider>,document.getElementById('root'));
 registerServiceWorker();
