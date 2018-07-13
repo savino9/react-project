05a550c815fa05652f472f82b70fc803ef79ddcf
Savino Rasola <savinorasola90@gmail.com>
2018-07-13 12:16:59 +0200

    Redux Middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // connecting React->Redux
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {createLogger} from 'redux-logger';
 // 
 
 import './index.css';
@@ -11,7 +12,9 @@ import registerServiceWorker from './registerServiceWorker';
 import {searchRobots} from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 
 ReactDOM.render(
 	<Provider store={store}>
